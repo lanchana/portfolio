d3eb28aa7d525c4fb4fc9ce8120121f622dd4213
lanchanashivananda <lanchanagupta@gmail.com>
2017-03-30 09:19:34 -0400

    Add background image

---
+++ js/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 33.899400, lng: -84.457890};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
