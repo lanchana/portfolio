aab2f4789e3977995271a6a9a0b48614053890d9
lanchanashivananda <lanchanagupta@gmail.com>
2017-04-15 22:47:36 -0400

    add contact and project info

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(() => {
     $(".button-collapse").sideNav();
+
+$("#thisForm").submit(function(event){
+      event.preventDefault();
+      $.ajax({
+        url: "https://www.enformed.io/ricbofc4",
+        method: "post",
+        dataType: "json",
+        accepts: "application/json",
+        data: $("#thisForm").serialize(),
+        success: function(){
+          console.log("Your form was successfully received!");
+        },
+        error: function(){
+          console.log("Failure. Try again.");
+        }
+      });
+    });
+
+$('.target').pushpin({
+      top: 0,
+      bottom: 1000,
+      offset: 0
+    });
 });
+
--- js/map.js
+++ js/map.js
@@ -1,11 +1,41 @@
 function initMap() {
-        var uluru = {lat: 33.899400, lng: -84.457890};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
+        // var uluru = {lat: 33.899400, lng: -84.457890};
+        var myCenter = new google.maps.LatLng(33.7490,-84.3880);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(33.7490,-84.3880),
+    zoom:15,
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+    mapTypeId:google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+  google.maps.event.addListener(marker,'click',function() {
+    var infowindow = new google.maps.InfoWindow({
+      content:"Atlanta, GA"
+    });
+  infowindow.open(map,marker);
+  });
+
+
+  //       var mapCanvas = document.getElementById("map");
+  // var mapOptions = {
+  //   center: new google.maps.LatLng(33.7490,-84.3880),
+  //   zoom:15,
+  //   mapTypeId:google.maps.MapTypeId.HYBRID
+  // };
+  // var map = new google.maps.Map(mapCanvas,mapOptions);
+  // var marker = new google.maps.Marker({position:myCenter});
+  // marker.setMap(map);
+  // google.maps.event.addListener(marker,'click',function() {
+  //   var infowindow = new google.maps.InfoWindow({
+  //     content:"Hello World!"
+  //   });
+  // infowindow.open(map,marker);
+  // });
       }
