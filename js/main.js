657e107f5b8c337f21f9bae003d945c8ef0efb21
lanchanashivananda <lanchanagupta@gmail.com>
2017-05-31 17:47:52 -0400

    comments and indent to javascript files

--- js/main.js
+++ js/main.js
@@ -1,56 +1,53 @@
-
-
 $(document).ready(() => {
 
+  // This initializes filterize
   $('.filtr-container').filterizr({});
 
+  // Mobile demo nav bar
   $(".button-collapse").sideNav({menuWidth: 280});
 
+  // Contact form
   $("#thisForm").submit(function(event){
     event.preventDefault();
-      $.ajax({
-          url: "https://www.enformed.io/ricbofc4",
-          method: "post",
-          dataType: "json",
-          accepts: "application/json",
-          data: $("#thisForm").serialize(),
-          success: function(){
-            console.log("Your form was successfully received!");
-            $("#thisForm").trigger('reset');
-          },
-          error: function(){
-            $("#thisForm").trigger('reset');
-            console.log("Failure. Try again.");
+    $.ajax({
+        url: "https://www.enformed.io/ricbofc4",
+        method: "post",
+        dataType: "json",
+        accepts: "application/json",
+        data: $("#thisForm").serialize(),
+        success: function(){
+          console.log("Your form was successfully received!");
+          $("#thisForm").trigger('reset');
+        },
+        error: function(){
+          $("#thisForm").trigger('reset');
+          console.log("Failure. Try again.");
+        }
+    });
+  });
 
-          }
-        });
-      });
-
-$('.modal').modal();
-// $('#modal1').modal('open');
-
-
-
+  // Modal initialization
+  $('.modal').modal();
 });
-
-
 
 window.onscroll = function() {scrollFunction()};
 
+// Smooth scroll
 function scrollFunction() {
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    // document.body.scrollTop = 0;
-    $('html, body').animate({
-        scrollTop: 0
-      }, 600);
-    document.documentElement.scrollTop = 0;
+  // document.body.scrollTop = 0;
+  $('html, body').animate({
+      scrollTop: 0
+    }, 600);
+
+  document.documentElement.scrollTop = 0;
 }
 
--- js/map.js
+++ js/map.js
@@ -1,7 +1,8 @@
+// Initializes google map api
 function initMap() {
-        // var uluru = {lat: 33.899400, lng: -84.457890};
-        var myCenter = new google.maps.LatLng(33.7490,-84.3880);
+  var myCenter = new google.maps.LatLng(33.7490,-84.3880);
   var mapCanvas = document.getElementById("map");
+
   var mapOptions = {
     center: new google.maps.LatLng(33.7490,-84.3880),
     zoom:15,
@@ -12,30 +13,18 @@ function initMap() {
     draggable: false,
     mapTypeId:google.maps.MapTypeId.HYBRID
   };
+
   var map = new google.maps.Map(mapCanvas, mapOptions);
   var marker = new google.maps.Marker({position:myCenter});
+
   marker.setMap(map);
+
+  // Displays location name on info window
   google.maps.event.addListener(marker,'click',function() {
     var infowindow = new google.maps.InfoWindow({
       content:"Atlanta, GA"
     });
-  infowindow.open(map,marker);
+
+    infowindow.open(map,marker);
   });
-
-
-  //       var mapCanvas = document.getElementById("map");
-  // var mapOptions = {
-  //   center: new google.maps.LatLng(33.7490,-84.3880),
-  //   zoom:15,
-  //   mapTypeId:google.maps.MapTypeId.HYBRID
-  // };
-  // var map = new google.maps.Map(mapCanvas,mapOptions);
-  // var marker = new google.maps.Marker({position:myCenter});
-  // marker.setMap(map);
-  // google.maps.event.addListener(marker,'click',function() {
-  //   var infowindow = new google.maps.InfoWindow({
-  //     content:"Hello World!"
-  //   });
-  // infowindow.open(map,marker);
-  // });
-      }
+}
