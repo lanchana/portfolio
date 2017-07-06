bc4cfcdc1be7a1d2dd8af8763420bdb307a868b8
lanchanashivananda <lanchanagupta@gmail.com>
2017-07-06 01:40:10 -0400

    Move index source file to index-src and create index min by using minify-html package

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require("gulp"),
     plumber = require("gulp-plumber"),
     uglyfly = require("gulp-uglyfly"),
     rename = require("gulp-rename"),
-    cssmin = require("gulp-uglifycss");
+    cssmin = require("gulp-uglifycss"),
+    htmlmin = require("gulp-minify-html");
 
 gulp.task("scripts", () => {
     gulp.src("js/*.js")
@@ -20,9 +21,18 @@ gulp.task("styles", () => {
         .pipe(gulp.dest("css/css-min/"));
 });
 
+gulp.task("content", () => {
+    gulp.src("index-src/index.html")
+        .pipe(htmlmin({
+            empty: true,
+            quotes: true
+        }))
+        .pipe(gulp.dest(""));
+})
+
 gulp.task("watch", () => {
     gulp.watch("js/*.js", ["scripts"]);
     gulp.watch("css/*.css", ["styles"]);
 });
 
-gulp.task("default", ["scripts", "watch", "styles"]);
+gulp.task("default", ["scripts", "styles", "content", "watch"]);
